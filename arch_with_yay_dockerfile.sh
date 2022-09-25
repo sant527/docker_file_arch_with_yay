37e3fed553d2cf6ec20fb121c7056fab739efbcf
Santhosh Kumar <sant527@users.noreply.github.com>
2022-09-25 13:55:41 +0530

    Update arch_with_yay_dockerfile.sh

--- arch_with_yay_dockerfile.sh
+++ arch_with_yay_dockerfile.sh
@@ -7,21 +7,19 @@ trap "rm $dockerfile" EXIT
 # we are escaping $ and \ with \$ and \\
 cat << EOF > $dockerfile
 FROM archlinux:base-devel-20220320.0.50753
+RUN echo "Server = https://archive.archlinux.org/repos/2022/03/25/\\\$repo/os/\\\$arch" > /etc/pacman.d/mirrorlist
 RUN yes | pacman -Syy
-
-# makepkg user and workdir
+RUN yes | pacman -Syu archlinux-keyring
+# Create user and sudo him
 # check the below step what to replace with what
 RUN sed -i 's@users:x:984:@users:x:1000:@g' /etc/group
 ARG user=simha
 RUN useradd --system -u 1000 -g 1000 -m \$user \\
   && echo "\$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/\$user
 RUN chown simha:users /home/simha
-
 USER \$user
 WORKDIR /home/\$user
-
 RUN yes |sudo pacman -S git --noconfirm
-
 # Install yay
 RUN git clone https://aur.archlinux.org/yay.git \\
   && cd yay \\
@@ -29,8 +27,14 @@ RUN git clone https://aur.archlinux.org/yay.git \\
   && cd \\
   # Clean up
   && rm -rf .cache yay
-CMD /bin/bash
-
+# install the required packages
+# # example yt-dl(it required python and ffmpeg)
+# RUN sudo pacman -S wget --needed --noconfirm
+# RUN sudo pacman -S curl --needed --noconfirm
+# RUN sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+# RUN sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
+# RUN sudo pacman -S ffmpeg --needed --noconfirm
+# RUN sudo pacman -S python --needed --noconfirm
 EOF
 
 docker build -t arch_with_yay - < $dockerfile
